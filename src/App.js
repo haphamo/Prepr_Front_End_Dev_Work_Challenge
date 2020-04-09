5065ab4af126c1b3d89f9cc10d3ed4b59f7b6564
haphamo <hapham0392@gmail.com>
2020-04-09 11:29:01 -0400

    created header with logo, search icon, search input and avatar icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import PreprLogo from './assets/Prepr-Logo.png'
+import searchIcon from './assets/search_icon.png'
+import user from './assets/default-user.png'
+import "./App.css";
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div className='header-container'>
+      <div className="logo-section">
+        <img id="preprLogo" alt="prepr-logo" src={PreprLogo}></img>
+      </div>
+      <div className="header-right-section">
+        <div className="search">
+          <img id="search-icon" alt="search-icon" src={searchIcon}></img>
+          <form>
+            <input id="search-input" type="search" autoFocus="true"></input>
+          </form>
+        </div>
+        <div className="profile">
+          <img alt="profile" src={user} id="profile"></img>
+        </div>
+
+      </div>
+
+    </div>
   );
 }
 

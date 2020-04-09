8127c642280425ada7e4160829d0fc8adbf4fbf5
haphamo <hapham0392@gmail.com>
2020-04-09 14:18:19 -0400

    refactored the key generator into separate helper function file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { navigationItems } from '../data/NavData.js';
+import { navigationItems } from '../data/Nav_Data.js';
 import "./Nav.css"
+import { randomKeyGenerator } from '../helpers/Helper_Functions'
 
 // map through data in NavData to get a list of all nav items
 const allNavItems = navigationItems.map(nav => {
-  const { name, src, alt, key} = nav
+  const { name, src, alt } = nav
   return(
-    <li key={key}>
+    <li key={randomKeyGenerator()}>
       <div className="nav-icon-">
         <img alt={alt} src={src}></img>
       </div>
---
+++ src/helpers/Helper_Functions.js
@@ -0,0 +1,1 @@
+export const randomKeyGenerator = () => Math.random().toString(36).substring(2, 5)

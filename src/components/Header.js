04ad3f1c4a97c50eb3ea854096af441a12bea25c
haphamo <hapham0392@gmail.com>
2020-04-09 11:36:11 -0400

    Moved header code into separate Header component

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PreprLogo from '../assets/Prepr-Logo.png'
+import searchIcon from '../assets/search_icon.png'
+import user from '../assets/default-user.png'
+
+export default function Header() {
+  return(
+    <div className='header-container'>
+    <div className="logo-section">
+      <img id="preprLogo" alt="prepr-logo" src={PreprLogo}></img>
+    </div>
+    <div className="header-right-section">
+      <div className="search">
+        <img id="search-icon" alt="search-icon" src={searchIcon}></img>
+        <form>
+          <input id="search-input" type="search" autoFocus="true"></input>
+        </form>
+      </div>
+      <div className="profile">
+        <img alt="profile" src={user} id="profile"></img>
+      </div>
+
+    </div>
+
+  </div>
+  )
+}

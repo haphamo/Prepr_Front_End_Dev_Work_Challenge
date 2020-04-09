90b1ad625838ab21eafc1b8affe87775b26ac5a7
haphamo <hapham0392@gmail.com>
2020-04-09 11:36:41 -0400

    moved header code in app to Header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import PreprLogo from './assets/Prepr-Logo.png'
-import searchIcon from './assets/search_icon.png'
-import user from './assets/default-user.png'
+import Header from './components/Header.js'
 import "./App.css";
 
 function App() {
   return (
-    <div className='header-container'>
-      <div className="logo-section">
-        <img id="preprLogo" alt="prepr-logo" src={PreprLogo}></img>
-      </div>
-      <div className="header-right-section">
-        <div className="search">
-          <img id="search-icon" alt="search-icon" src={searchIcon}></img>
-          <form>
-            <input id="search-input" type="search" autoFocus="true"></input>
-          </form>
-        </div>
-        <div className="profile">
-          <img alt="profile" src={user} id="profile"></img>
-        </div>
-
-      </div>
-
-    </div>
+    <Header />
   );
 }
 

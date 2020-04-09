d7b9ebc7f85b56c58702fa5efcd70c4ff6cbb87e
haphamo <hapham0392@gmail.com>
2020-04-09 15:17:27 -0400

    Created card component for the challenges/labs/projects and imported into App to test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header.js'
 import Nav from './components/Nav.js'
+import Card from './components/Card.js'
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <Nav />
+      <Card />
     </div>
   );
 }

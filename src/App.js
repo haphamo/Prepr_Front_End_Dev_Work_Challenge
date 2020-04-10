5eb5ff23fbfcc77d6442fd185191cd1de3bf8e21
haphamo <hapham0392@gmail.com>
2020-04-10 14:16:52 -0400

    imported CategoryButton component into App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Header from './components/Header.js'
-import Nav from './components/Nav.js'
-import Card from './components/Card.js'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/Header.js";
+import Nav from "./components/Nav.js";
+import Card from "./components/Card.js";
+import CategoryButton from "./components/CategoryButton.js";
 import "./App.css";
-
 
 function App() {
   return (
     <div>
       <Header />
       <Router>
-      <Nav />
+        <Nav />
+        <CategoryButton />
         <Switch>
           <Route exact path="/">
-            <Card  type="challenge"/>
+            <Card type="challenge" />
           </Route>
           <Route path="/labs">
-            <Card type="lab"/>
+            <Card type="lab" />
           </Route>
           <Route path="/projects">
-            <Card type="project"/>
+            <Card type="project" />
           </Route>
         </Switch>
       </Router>
-
     </div>
   );
 }

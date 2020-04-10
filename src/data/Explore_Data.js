0ee5f98a59b27e99f0b58febb5749a5256f0104c
haphamo <hapham0392@gmail.com>
2020-04-09 23:22:06 -0400

    Added data for challenges in explore page

---
+++ src/data/Explore_Data.js
@@ -0,0 +1,75 @@
+import c1 from '../assets/c1.jpg'
+import c2 from '../assets/c2.jpg'
+import c3 from '../assets/c3.png'
+import c4 from '../assets/c4.png'
+import c5 from '../assets/c5.png'
+import c6 from '../assets/c6.png'
+
+export const data = {
+  1:  {
+        id: 1,
+        type: 'challenge',
+        status: 'open',
+        src: c1,
+        title: 'COVID-19 Challenge - Low-cost monitoring system for COVID-19 patients',
+        summary: 'NRC is seeking a low cost system (unit cost <$25) that can continuously measure temperature, peripheral capillary oxygen saturation (SpO2), blood pressure (BP), pulse and respiration rates, and transmit this information wirelessly to a base station for COVID-19 patient monitoring in locales including ERs, general wards, communities and homes.',
+        tags: ['tagOne', 'tagTwo'],
+        startDate: 'April 7th, 2020',
+        endDate: 'April 20th, 2020'
+      },
+  2:  {
+        id: 2,
+        type: 'challenge',
+        status: 'open',
+        src: c2,
+        title: 'COVID-19 Challenge - Point of Care & Home Diagnostic Kit for COVID-19',
+        summary: 'The Public Health Agency of Canada (PHAC) and National Research Council (NRC) are seeking a solution that will diagnose individuals affected by COVID-19 within 3 days of the start of their symptoms using a sample, other than a nasopharyngeal swab, with a rapid single-use home testing kit analogous to a home pregnancy test.',
+        tags: ['tagOne', 'tagTwo'],
+        startDate: 'April 7th, 2020',
+        endDate: 'April 20th, 2020'
+      },
+  3:  {
+        id: 3,
+        type: 'challenge',
+        status: 'open',
+        src: c3,
+        title: 'Recycled Plastic Ceiling Tiles',
+        summary: 'Global Affairs Canada is seeking a solution that will contribute to the circular economy by recycling plastic into ceiling tiles and that meet a high safety standard with no chemical emissions. The existing ceiling tiles are drop ceiling mineral acoustical tiles. We need something comparable in acoustical, insulating and fire retardant performance, but made with 100% recycled plastic.',
+        tags: ['tagOne', 'tagTwo'],
+        startDate: 'April 7th, 2020',
+        endDate: 'April 20th, 2020'
+      },
+  4:  {
+        id: 4,
+        type: 'challenge',
+        status: 'open',
+        src: c4,
+        title: 'Diverting End of Life Vehicle Plastics from Landfills',
+        summary: 'The National Research Council of Canada (NRC) and Environment and Climate Change Canada (ECCC) are seeking environmentally acceptable and cost-effective technologies that will enable the diversion of End of Life Vehicles (ELVs) plastics from landfills and their conversion into valuable materials and/or molecules.',
+        tags: ['tagOne', 'tagTwo'],
+        startDate: 'April 7th, 2020',
+        endDate: 'April 20th, 2020'
+      },
+  5:  {
+        id: 5,
+        type: 'challenge',
+        status: 'open',
+        src: c5,
+        title: 'Sensing Technology for Monitoring Microplastics in Marine Environments',
+        summary: 'The National Research Council of Canada (NRC) is seeking an innovative in situ (on site) sensing technology for identification and quantification of microplastics in water, which will enable the development of new techniques for monitoring microplastics in Canadian waters.',
+        tags: ['tagOne', 'tagTwo'],
+        startDate: 'April 7th, 2020',
+        endDate: 'April 20th, 2020'
+      },
+  6:  {
+        id: 6,
+        type: 'challenge',
+        status: 'open',
+        src: c6,
+        title: 'E-waste',
+        summary: 'Shared Services Canada (SSC) and Environment and Climate Change Canada (ECCC) are seeking a solution that will enable recycling of e-waste plastics and metals in Canada while respecting domestic and international requirements and obligations.',
+        tags: ['tagOne', 'tagTwo'],
+        startDate: 'March 5th, 2020',
+        endDate: 'April 14th, 2020'
+      },
+}

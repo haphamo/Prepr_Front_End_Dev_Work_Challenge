6db757c966ea8eeda4051475cff692fce8b420fe
haphamo <hapham0392@gmail.com>
2020-04-10 14:21:43 -0400

    Added comments in Helper Functions

--- src/helpers/Helper_Functions.js
+++ src/helpers/Helper_Functions.js
@@ -1,14 +1,17 @@
+// Creates random key/id for components created using map
 const randomKeyGenerator = () => Math.random().toString(36).substring(2, 5)
 
+// Creates a shortened summary to display on the cards, like a preview
 const shortenedSummaryForCardDisplay = function(summary) {
   return `${summary.slice(0, 150)}...`
 }
 
+// This function receives a data obj of all the challenges, projects and labs as objects. It will return an array of the specific type passed in
 const getSpecificData = function(data, type) {
-  
   return Object.values(data).filter(item => item.type === type) 
 }
 
+// All the nav icons and their data is stored here. The filtered Nav returns an array that is used in the navigation bar. The 3 that return false are the categories
 const filteredNav = function(navItem) {
   switch(navItem.name) {
     case "Challenges":
